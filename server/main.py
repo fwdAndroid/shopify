eb7c6f72569b9292eb99eb1b773901d2f933e272
fwdAndroid <fwdkaleem@gmail.com>
2025-07-09 17:36:24 +0500

    SignUp Api

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 # Database connection setup
@@ -7,6 +7,9 @@ from sqlalchemy.orm import sessionmaker
 
 #Database connection
 from sqlalchemy.ext.declarative import declarative_base
+
+import uuid
+import bcrypt
 
 app = FastAPI()
 
@@ -41,8 +44,18 @@ class User(Base):
 def signup_user(user: UserCreate):
     # extract user data from request
     print(user.name, user.email, user.password)
-    # Here you would typically hash the password and save the user to the database
-    pass
+    # Here you check user avaialbe yes or no
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if user_db:
+        raise HTTPException(status_code=400, detail="User already exists")
+    # If user does not exist, create a new user
+    hashed_pw = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    user_db = User(id=str(uuid.uuid4),email=user.email,name=user.name,password=hashed_pw)
+
+    db.add(user_db)
+    db.commit()
+    db.refresh(user_db)
+    return user_db
 
 # Create the database tables    
 Base.metadata.create_all(engine)
